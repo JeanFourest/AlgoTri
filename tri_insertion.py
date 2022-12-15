87bade8fd81e7f7a8944137212dbbd2071c07085
Alexandre <116552630+Creepynax@users.noreply.github.com>
2022-12-15 14:26:51 +0100

    add compteur in tri_insertion.py

---
+++ tri_insertion.py
@@ -0,0 +1,23 @@
+def tri_insertion(tab):
+    
+    echanges_insertion = 0
+    comparaisons_insertion = 0
+    
+    for i in range(1, len(tab)):
+        k = tab[i]
+        j = i - 1
+        
+        while j >= 0 and k < tab[j]:
+            tab[j + 1] = tab[j]
+            j -= 1
+            comparaisons_insertion += 1
+        echanges_insertion += 1
+        tab[j + 1] = k
+        
+        
+    affectations_insertion = echanges_insertion * 3    
+    return tab, comparaisons_insertion, echanges_insertion, affectations_insertion
+
+tab = [10, 9, 8, 7]
+
+print(tri_insertion(tab))
