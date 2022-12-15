0e6cfafbe5ac1b2a71624891b6573c99fcdc703d
Alexandre <116552630+Creepynax@users.noreply.github.com>
2022-12-15 18:02:43 +0100

    create main.py
    Création de main.py pour y relier nos différentes fonctions

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import tri_insertion
 import tri_selection 
 from random import randint
 
-tab = [273, 8732, 81, 0, 823, 6, 354, 12, 56]
+tab = [1, 5, 2, 4, 3]
 
-print(tri_selection.tri_selection(tab))
+#print(tri_selection.tri_selection(tab))
 print(tri_insertion.tri_insertion(tab))
-print(tri_bulle_norm.tri_bulle_norm(tab))
-print(tri_bulle_opti.tri_bulle_opti(tab))
+#print(tri_bulle_norm.tri_bulle_norm(tab))
+#print(tri_bulle_opti.tri_bulle_opti(tab))
