9940d1ab2ef210634ad50756fb1cb46f961e26f6
Alexandre <116552630+Creepynax@users.noreply.github.com>
2022-12-15 10:11:36 +0100

    add comments

--- tri_selection.py
+++ tri_selection.py
@@ -1,20 +1,19 @@
 # Création de la fonction de tri sélection 
 def tri_selection(tab):
-    compteur = 0
-    for i in range(len(tab) - 1):
+
+    # Boucle qui parcourt la longueur du tableau donné
+    for i in range(len(tab)):
 
         min = i
 
+        # Boucle qui compare la valeur prise en minimum à l'ensemble des valeurs du tableau
         for j in range(i+1, len(tab)):
             if tab[min] > tab[j]:
                 min = j
 
+        
         tempo = tab[i]
         tab[i] = tab[min]
         tab[min] = tempo
         
     return tab
-
-tab = [69, 10, 20, 6, 4, 12]
-
-print(tri_selection(tab))
